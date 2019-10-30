059ceb4631db1c4d89d1563ac779e9b7c7801aee
George Benjamin <msdcconnect@gmail.com>
2019-10-30 03:12:35 +0100

    Removed hardcoded api urls

--- src/api/search.js
+++ src/api/search.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-
+import constants from '../constants';
 const parseResponse = (response) => response.data
 
 export default {
     search: async function (query, page) {
-      let results = await fetch(`http://localhost:3000/search?search=${query}&page=${page}`);
+      let results = await fetch(`${constants.API_URL}/search?search=${query}&page=${page}`);
       results = await results.json();
       return results;
     },
     getIndexedLinksCount: async function(){
-      let result = await axios.get('http://localhost:3000/count');
+      let result = await axios.get(`${constants.API_URL}/count`);
       return parseResponse(result)
     }
 }
